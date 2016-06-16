ae62e739db5d6ab34ff630dc665175408ba8bdb3
Joe Wallis <joe@thewallisfamily.co.uk>
2016-06-16 01:42:33 +0100

    Initial upload

---
+++ hrm/levels.py
@@ -0,0 +1,19 @@
+import hrm
+
+def _level(*args, **kwargs):
+    def wrapper(program):
+        return hrm.HRM(program, *args, **kwargs)
+    return wrapper
+
+level1 = _level()
+level2 = _level()
+level3 = _level(6, {0: 'U', 1: 'J', 2: 'X', 3: 'G', 4: 'B', 5: 'E'})
+level4 = _level(3)
+level6 = _level(3)
+level7 = _level(9)
+level8 = _level(3)
+level9 = _level(9)
+level10 = _level(5)
+level11 = _level(3)
+level12 = _level(5)
+level41 = _level(25, {24: 0})
